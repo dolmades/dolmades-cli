f8c77bf92dc72a9b4226ff2ce0de2daeb206d877
Stefan Kombrink <katakombi@gmail.com>
2018-10-19 15:49:29 +0200

    implement init

--- config.py
+++ config.py
@@ -19,6 +19,49 @@ UDOCKER = SELF_PATH+"/udocker"
 UDOCKERCMD_VERBOSE = UDOCKER+" --repo="+REPO_PATH
 UDOCKERCMD_QUIET = UDOCKER+" --quiet --repo="+REPO_PATH
 
+def INIT():
+    print("Preparing directories...")
+
+    untouched = True
+    if not os.path.exists(DOLMADES_PATH):
+        os.mkdir(DOLMADES_PATH, 0755)
+        untouched = False
+    try:
+        if (os.path.exists(REPO_PATH)):
+            os.rmdir(REPO_PATH)
+            untouched = False
+    except:
+        pass
+
+    if not os.path.exists(REPO_PATH):
+        cmd = UDOCKER+ " mkrepo "+REPO_PATH
+        print(cmd)
+        subprocess.call(cmd, shell=True, close_fds=True)
+        untouched = False
+
+    if (not os.path.exists(INST_PATH)):
+        os.mkdir(self.INST_PATH, 0755)
+        untouched = False
+    if (untouched):
+        print("found dolmade repo under " + REPO_PATH)
+    else:
+        print("initialized dolmade repo under " + REPO_PATH)
+
+    print("Preparing runtime...")
+    cmd = UDOCKERCMD_QUIET+" pull dolmades/runtime:"+VERSION
+    print(cmd)
+    print("Pulling dolmades runtime container...")
+    subprocess.call(cmd, shell=True, close_fds=True)
+
+    cmd = UDOCKERCMD_QUIET+" rm dolmades-runtime"
+    print(cmd)
+    subprocess.call(cmd, shell=True, close_fds=True)
+
+    cmd = UDOCKERCMD_QUIET+" create --name=dolmades-runtime dolmades/runtime:"+VERSION
+    print(cmd)
+    subprocess.call(cmd, shell=True, close_fds=True)
+    print("done")
+
 try:
     DESK_PATH = subprocess.check_output(['xdg-user-dir', 'DESKTOP']).strip()
 except:
