5d2c43848ec078559b76fe4bf7f2470c5416cdec
Stefan Kombrink <katakombi@gmail.com>
2019-04-22 23:10:40 +0200

    added: support for S1,P1,P2 container execution engines / autodetection of reliably working engines / command "setup" to get/set execution engines for dolmades

--- config.py
+++ config.py
@@ -2,9 +2,11 @@ import sys
 import os
 import subprocess
 import getpass
+import shlex
 
 from prettyprint import printitb
 from platform import architecture
+from prettyprint import printx
 
 # Python version major.minor
 PY_VER = "%d.%d" % (sys.version_info[0], sys.version_info[1])
@@ -26,6 +28,9 @@ UDOCKER = SELF_PATH+"/udocker"
 UDOCKERCMD_VERBOSE = UDOCKER
 UDOCKERCMD_QUIET = UDOCKER+" --quiet"
 
+UDOCKER_ENGINE="auto" # valid options: auto/S1/P1/P2
+UDOCKER_ENGINE_PREFERENCE=['S1','P1','P2']
+
 os.environ["UDOCKER_DIR"]=DOLMADES_PATH
 # This is set for caching winetricks ingredients
 os.environ["XDG_CACHE_HOME"]=INGREDIENTS_PATH
@@ -39,6 +44,36 @@ else:
     DOLMADE_SUFFIX=""
 
 
+def SETUP(dolmaname, mode=None):
+    enginePreference=UDOCKER_ENGINE_PREFERENCE
+    if (mode == None):
+        mode = UDOCKER_ENGINE
+    try:
+        if (mode=="auto"):
+            print("Trying to setup the best container engine...")
+            for engine in enginePreference:
+                cmdline=UDOCKERCMD_QUIET+" setup --execmode="+engine+" "+dolmaname
+                print(cmdline)
+                subprocess.call(cmdline, shell=True, close_fds=True)
+                cmdline=UDOCKERCMD_QUIET+" run --user=$(whoami) "+dolmaname+" sleep 0"
+                print(cmdline)
+                subprocess.call(cmdline, shell=True, close_fds=True)
+                safe_cmd=shlex.split(cmdline)
+                proc=subprocess.Popen(safe_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                out, err = proc.communicate()
+                if (proc.returncode==0):
+                   mode=engine
+                   print("Success using "+mode)
+                   return
+                raise Exception("None of the available container engines is usable")
+        if mode not in enginePreference:
+            raise Exception("Unsupported container engine: " + mode)
+        else:
+            cmdline=UDOCKERCMD_QUIET+" setup --execmode="+mode+" "+dolmaname
+            print(cmdline)
+            subprocess.call(cmdline, shell=True, close_fds=True)
+    except Exception as error:
+        printx(error)
 
 def INIT(force):
     printitb("Initializing dolmades under "+DOLMADES_PATH+"...")
@@ -85,6 +120,8 @@ def INIT(force):
         cmd = UDOCKERCMD_QUIET+" create --name=dolmades-runtime "+RUNTIME_IMAGE+":"+VERSION
         print(cmd)
         subprocess.call(cmd, shell=True, close_fds=True)
+
+        self.SETUP("dolmades-runtime")
 
     if (not os.path.exists(DOLMADES_PATH+"/containers/dolmades-runtime/ROOT/"+META_DIR)):
         os.mkdir(DOLMADES_PATH+"/containers/dolmades-runtime/ROOT/"+META_DIR, 0755)
